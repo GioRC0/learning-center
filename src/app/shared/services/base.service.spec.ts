5dba43d73ffc9cb66179bc531f62b8346d288352
wutdubs <u201817841@upc.edu.pe>
2023-09-12 09:47:46 -0500

    chore(environment): created environment directory

--- src/app/shared/services/base.service.spec.ts
+++ src/app/shared/services/base.service.spec.ts
@@ -1,7 +1,8 @@
 import { BaseService } from './base.service';
 
 describe('BaseService', () => {
-  it('should create an instance', () => {
-    expect(new BaseService()).toBeTruthy();
-  });
+  it('should create an instance',
+    () => {
+      expect(new BaseService()).toBeTruthy();
+    });
 });
