0bbb3734d276775ce07b7743e1cb576bd0d1ccb9
wutdubs <u201817841@upc.edu.pe>
2023-09-12 09:10:06 -0500

    chore(students): added students service facade as a provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './learning/pages/students/students.component';
+import {HttpClientModule} from "@angular/common/http";
+import {StudentsService} from "./learning/services/students.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StudentsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
